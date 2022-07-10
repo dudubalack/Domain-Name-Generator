5dda1645b751010f64c23a81e7682d7ab94d76f1
dudubalack <dominguesdatrindadecarloseduar@gmail.com>
2022-07-10 15:24:43 +0000

    ejercicio listo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extencion = [".com"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let m = 0; m < adj.length; m++) {
+      for (let v = 0; v < noun.length; v++) {
+        for (let x = 0; x < extencion.length; x++) {
+          console.log(pronoun[i] + adj[m] + noun[v] + extencion[x]);
+        }
+      }
+    }
+  }
 };
